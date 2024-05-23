fb30373d1020d14605c8f3545160e0b7fcb80c91
Franco <109772454+FrancoMoya@users.noreply.github.com>
2024-05-23 17:11:06 -0300

    Cambio de menú

--- MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c091b523b38f3b77423fc4bde0913b0c94e2a12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e700b884451b7a65a2d4262a9eb98130b66a06a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
